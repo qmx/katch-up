101791040a91b8c436994b7a21d4d75c8cf77c6b
Douglas Campos <douglas@theros.info>
2010-06-09 20:38:32 -0300

    root folder

--- lib/katch_up/app.rb
+++ lib/katch_up/app.rb
@@ -3,6 +3,7 @@ require 'haml'
 
 module KatchUp
 	class App < Sinatra::Base
+		set :root, File.expand_path("../../..", __FILE__)
 		get '/' do
 			@worked_hours = Base.new.expected_hours	
 			haml :index
