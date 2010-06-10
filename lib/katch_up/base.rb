17d9b9282ed85d0c9a5450b0b23465b952ab766a
Douglas Campos <douglas@theros.info>
2010-06-10 01:06:39 -0300

    paranoid setting time as utc

--- lib/katch_up/base.rb
+++ lib/katch_up/base.rb
@@ -1,11 +1,11 @@
 module KatchUp
 	class Base
 		def expected_hours
-			weekdays(Time.now) * 8
+			weekdays(Time.now.utc) * 8
 		end
 
-		def weekdays(now = Time.now.end_of_month)
-			now.beginning_of_month.weekdays_until(now.to_date)
+		def weekdays(now = Time.now.utc.end_of_month)
+			now.utc.beginning_of_month.weekdays_until(now.utc.to_date)
 		end
 	end
 end
